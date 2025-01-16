938018bb3d176fe64baf19777cc0732690a5ed0d
Shloka Trivedi <shlokatrivedi@Shlokas-MacBook-Air.local>
2025-01-16 17:33:05 -0500

    demo a1

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Labs from './Labs/Lab3' 
 function App() {
   const [count, setCount] = useState(0)
 
@@ -17,17 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Welcome to Full Stack MERN Development</h1>
+      <div><Labs /></div>
     </>
   )
 }
