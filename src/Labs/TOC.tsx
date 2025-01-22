f437bbfb10197c1d2f208586b5c427a3ab9d7054
Shloka Trivedi <shlokatrivedi@Shlokas-MacBook-Air.local>
2025-01-22 15:11:10 -0500

    staging and pushing github link

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -5,6 +5,7 @@ export default function TOC() {
       <li><a id="wd-a2" href="#/Labs/Lab2">Lab 2</a></li>
       <li><a id="wd-a3" href="#/Labs/Lab3">Lab 3</a></li>
       <li><a id="wd-k"  href="#/Kambaz">Kambaz</a></li>
+      <li><a id="github-link" href="https://github.com/Trivedi-s/webd-react-web-app-cs5610-sp25/tree/a1" target="_blank">Github repo</a></li>
     </ul>
   );
 }
