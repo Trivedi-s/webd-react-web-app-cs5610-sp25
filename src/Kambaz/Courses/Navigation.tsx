1abd9d2a623fec048e547df5e82e941e747350e6
Shloka Trivedi <shlokatrivedi@Shlokas-MacBook-Air.local>
2025-02-19 16:04:16 -0500

    a3 assignment editor changes

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -19,36 +19,49 @@
 //     </div>
 // );}
 
-import { Link, useLocation, useParams } from "react-router-dom";
+import { courses } from "../Database";
+import { ListGroup } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
+import { useParams } from "react-router";
 
-export default function CoursesNavigation() {
+export default function CourseNavigation() { 
   const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);
+
   const { pathname } = useLocation();
 
+  if (!course) {
+    return <p className="text-danger">Course not found!</p>;
+  }
+
   const links = [
-    "Home",
-    "Modules",
-    "Piazza",
-    "Zoom",
-    "Assignments",
-    "Quizzes",
-    "Grades",
-    "People",
-    "Settings"
+    { label: "Home", path: `/Kambaz/Courses/${course._id}/Home` },
+    { label: "Modules", path: `/Kambaz/Courses/${course._id}/Modules` },
+    { label: "Piazza", path: `/Kambaz/Courses/${course._id}/Piazza` },
+    { label: "Zoom", path: `/Kambaz/Courses/${course._id}/Zoom` },
+    { label: "Assignments", path: `/Kambaz/Courses/${course._id}/Assignments` },
+    { label: "Quizzes", path: `/Kambaz/Courses/${course._id}/Quizzes` },
+    { label: "Grades", path: `/Kambaz/Courses/${course._id}/Grades` },
+    { label: "People", path: `/Kambaz/Courses/${course._id}/People` },
+    { label: "Settings", path: `/Kambaz/Courses/${course._id}/Settings` }
   ];
 
-  return (
-    <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      {links.map((link) => {
-        const path = `/Kambaz/Courses/${cid}/${link}`;
-        const isActive = pathname === path;
-        return (
-          <Link key={link} to={path} className={`list-group-item border border-0 ${isActive ? "active" : "text-danger"}`}>
-            {link}
-          </Link>
-        );
-      })}
+  return ( 
+    <div>
+      
+      <ListGroup id="wd-courses-navigation" className="wd list-group fs-5 rounded-0"> 
+        {links.map((link) => (
+          <ListGroup.Item 
+            key={link.path} 
+            as={Link} 
+            to={link.path} 
+            className={`list-group-item border border-0 
+              ${pathname.includes(link.label) ? "active border border-0" : "text-danger border border-0"}`}
+          >
+            {link.label}
+          </ListGroup.Item>
+        ))}
+      </ListGroup> 
     </div>
-  );
+  ); 
 }
-
