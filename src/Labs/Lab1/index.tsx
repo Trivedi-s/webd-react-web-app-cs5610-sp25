b48ee42e9b0d402630b8446504cf3ae596a668a7
Shloka Trivedi <shlokatrivedi@Shlokas-MacBook-Air.local>
2025-02-19 16:24:32 -0500

    a3 name in lab

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,7 +1,6 @@
 export default function Lab1() {
     return (
         <div id="wd-lab1">
-            <h2>Shloka Shreyans Trivedi <br />Section: 01</h2>
             <h2>Lab 1: Building React User Interfaces with HTML, Assignment 1, Setting Up the Development Environment, Introduction to HTML</h2>
             <h3>HTML Examples</h3> 
 
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,7 @@ export default function Labs() {
   return (
     <div id="wd-labs">
             <h1>Labs</h1>
+            <h2>Shloka Shreyans Trivedi <br />Section: 01</h2>
             <TOC /> 
             <Routes> 
                 <Route path="/" element={<Navigate to="Lab1" />} /> 
