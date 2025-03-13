1239964ed2ac26dc955c85d72d5ae7466aa64703
Shloka Trivedi <shlokatrivedi@Shlokas-MacBook-Air.local>
2025-03-13 00:02:38 -0400

    a4 last

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -53,30 +53,79 @@
 //   );}
 
 
+// import CourseNavigation from "./Navigation";
+// import Modules from "./Modules";
+// import Home from "./Home";
+// import Assignments from "./Assignment";
+// import AssignmentEditor from "./Assignment/Editor";
+// import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
+// import { FaAlignJustify } from "react-icons/fa6";
+// import PeopleTable from "./People/Table";
+// import { useSelector } from "react-redux";
+// import AssignmentEditorViewOnly from "./Assignment/AssignmentEditorViewOnly";
+// export default function Courses() {
+//   const { cid } = useParams();
+//   const { courses } = useSelector((state: any) => state.coursesReducer);
+//   const course = courses.find((course: { _id: string | undefined; }) => course._id === cid);
+//   const { pathname } = useLocation();
+//   return (
+//     <div id="wd-courses">
+//       <h2 className="text-danger"> <FaAlignJustify className="me-4 fs-4 mb-1" />{course && course.name} &gt; {pathname.split("/")[4]}</h2>
+//       <hr />
+//       <div className="d-flex"> <div className="d-none d-md-block">
+//         <CourseNavigation />
+//       </div> <div className="flex-fill">
+//           <Routes>
+//             <Route path="/" element={<Navigate to="Home" />} />
+//             <Route path="Home" element={<Home />} />
+//             <Route path="Modules" element={<Modules />} />
+//             <Route path="Piazza" element={<h2>Piazza</h2>} />
+//             <Route path="Zoom" element={<h2>Zoom</h2>} />
+//             <Route path="Assignments" element={<Assignments />} />
+//             <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+//             <Route path="Assignments/:aid/AssignmentEditorViewOnly" element={<AssignmentEditorViewOnly />} />
+//             <Route path="Quizzes" element={<h2>Quizzes</h2>} />
+//             <Route path="Grades" element={<h2>Grades</h2>} />
+//             <Route path="People" element={<PeopleTable />} />
+//             <Route path="Settings" element={<h2>Settings</h2>} />
+//             <Route path="AssignmentEditor" element={<AssignmentEditor />} />
+//           </Routes>
+//         </div></div>
+//     </div>
+//   );
+// }
+
 import CourseNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignment";
 import AssignmentEditor from "./Assignment/Editor";
 import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
-//import { courses } from "../Database";
+import { useSelector } from "react-redux"; 
 import { FaAlignJustify } from "react-icons/fa6";
 import PeopleTable from "./People/Table";
-//import AssignmentEditorView from "./Assignment/AssignmentEditorViewOnly";
-import { useSelector } from "react-redux";
 import AssignmentEditorViewOnly from "./Assignment/AssignmentEditorViewOnly";
+
 export default function Courses() {
   const { cid } = useParams();
-  const { courses } = useSelector((state: any) => state.coursesReducer);
-  const course = courses.find((course: { _id: string | undefined; }) => course._id === cid);
   const { pathname } = useLocation();
+  
+  const courses = useSelector((state: any) => state.coursesReduccer.courses);
+
+  const course = courses.find((course: any) => course._id === cid);
+
   return (
     <div id="wd-courses">
-      <h2 className="text-danger"> <FaAlignJustify className="me-4 fs-4 mb-1" />{course && course.name} &gt; {pathname.split("/")[4]}</h2>
+      <h2 className="text-danger">
+        <FaAlignJustify className="me-4 fs-4 mb-1" />
+        {course ? course.name : "Course Not Found"} &gt; {pathname.split("/")[4]}
+      </h2>
       <hr />
-      <div className="d-flex"> <div className="d-none d-md-block">
-        <CourseNavigation />
-      </div> <div className="flex-fill">
+      <div className="d-flex">
+        <div className="d-none d-md-block">
+          <CourseNavigation />
+        </div>
+        <div className="flex-fill">
           <Routes>
             <Route path="/" element={<Navigate to="Home" />} />
             <Route path="Home" element={<Home />} />
@@ -92,7 +141,8 @@ export default function Courses() {
             <Route path="Settings" element={<h2>Settings</h2>} />
             <Route path="AssignmentEditor" element={<AssignmentEditor />} />
           </Routes>
-        </div></div>
+        </div>
+      </div>
     </div>
   );
 }
