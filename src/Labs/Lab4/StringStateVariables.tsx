e22d397ccd46af47d10eb517bcb7ba1e86ab3dff
Shloka Trivedi <shlokatrivedi@Shlokas-MacBook-Air.local>
2025-03-12 21:47:42 -0400

    a4 assignmenteditor change

--- src/Labs/Lab4/StringStateVariables.tsx
+++ src/Labs/Lab4/StringStateVariables.tsx
@@ -8,5 +8,6 @@ export default function StringStateVariables() {
       <p>{firstName}</p>
       <FormControl
         defaultValue={firstName}
-        onChange={(e) => setFirstName(e.target.value)}/>
-<hr/></div>);}
+        onChange={(e) => setFirstName(e.target.value)} />
+      <hr /></div>);
+}
