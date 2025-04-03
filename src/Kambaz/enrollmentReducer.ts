cde4aac5130f8f0e33b26de299575e795fed7e94
Shloka Trivedi <shlokatrivedi@Shlokas-MacBook-Air.local>
2025-04-03 19:35:45 -0400

    init done

---
+++ src/Kambaz/enrollmentReducer.ts
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { enrollments } from "./Database";
+import { v4 as uuidv4 } from 'uuid';
+
+const getInitialEnrollments = () => {
+    const storedEnrollments = sessionStorage.getItem('enrollments');
+    return storedEnrollments ? JSON.parse(storedEnrollments) : enrollments;
+};
+
+const initialState = {
+    enrollments: getInitialEnrollments(),
+};
+
+const enrollmentsSlice = createSlice({
+    name: "enrollments",
+    initialState,
+    reducers: {
+        unenroll: (state, { payload: { user, course } }) => {
+            state.enrollments = state.enrollments.filter((e: { user: any; course: any }) =>
+                !(e.course === course._id && e.user === user._id)
+            );
+
+            sessionStorage.setItem('enrollments', JSON.stringify(state.enrollments));
+
+            console.log("After unenroll:", JSON.stringify(state.enrollments, null, 2));
+        },
+        enroll: (state, { payload: { user, course } }) => {
+            const newEnrollment = {
+                _id: uuidv4(),
+                user: user._id,
+                course: course._id
+            };
+            state.enrollments = [...state.enrollments, newEnrollment];
+            sessionStorage.setItem('enrollments', JSON.stringify(state.enrollments));
+        },
+    },
+});
+
+export const { unenroll, enroll } = enrollmentsSlice.actions;
+export default enrollmentsSlice.reducer;
